7bd1e9b423eeb732b99205eccb1b4197dc935c4b
Yaima Martinez <ymtnez2@gmail.com>
2025-07-07 20:04:01 -0400

    Implement Private Route for Dashboard Routes

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 import { LoadingPage } from "@/pages/LoadingPage";
+import RequireAuth from "@/features/auth/components/PrivateRoute";
 
 const DashboardRouter = lazy(() => import("@/routes/DashboardRouter").then(module => ({ default: module.DashboardRouter })));
 const AuthRouter = lazy(() => import("@/routes/AuthRouter").then(module => ({ default: module.AuthRouter })));
@@ -11,7 +12,7 @@ export const AppRouter = () => (
         <Suspense fallback={<LoadingPage />}>
             <Routes>
                 <Route path="/account/*" element={<AuthRouter />} />
-                <Route path="/*" element={<DashboardRouter />} />
+                <Route path="/*" element={<RequireAuth> <DashboardRouter /> </RequireAuth>} />
 
                 <Route path="*" element={<Navigate to="/account/login" replace />} />
             </Routes>
